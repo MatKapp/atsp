7e1b43cba4acd0b317a6ba4d7ba7cf29f1684d0c
Jakub Sztyma <jakubsztyma@interia.pl>
2019-10-17 15:57:17 +0100

    Add best known solutions

--- main.go
+++ main.go
@@ -7,7 +7,37 @@ import (
 
 type fn func([][]int) ([]int, int)
 
+
 func main() {
+	bestKnownSolutions := map[string]int{
+		"br17": 39,
+		"ft53": 6905,
+		"ftv33": 1286,
+		"ft70": 38673,
+		"ftv35": 1473,
+		"ftv44": 1613,
+		"ftv38": 1530,
+		"ftv55": 1608,
+		"ftv47": 1776,
+		"ftv64": 1839,
+		"ftv70": 1950,
+		"ftv100": 1788,
+		"ftv110": 1958,
+		"ftv90": 1579,
+		"ftv120": 2166,
+		"ftv130": 2307,
+		"ftv140": 2420,
+		"ftv150": 2611,
+		"ftv170": 2755,
+		"ftv160": 2683,
+		"kro124": 36230,
+		"p43": 5620,
+		"rbg358": 1163,
+		"rbg323": 1326,
+		"rbg403": 2465,
+		"rbg443": 2720,
+		"ry48p": 14422,
+	}
 	instanceFilenames := []string{
 		"br17",
 		"ft53",
@@ -20,10 +50,16 @@ func main() {
 		"ftv55",
 		"ftv70",
 	}
+
 	for _, filename := range instanceFilenames {
+		fmt.Println()
 		fmt.Println(filename)
 		path := "data/" + filename + ".atsp"
 		distances := readData(path)
+
+		bestKnown := bestKnownSolutions[filename]
+		fmt.Println("Best known: ", bestKnown)
+
 		compute(solveHeuristic, distances, "Heuristic")
 		compute(solveGreedy, distances, "Greedy")
 		steepestElapsed := compute(solveSteepest, distances, "Steepest")
@@ -44,10 +80,11 @@ func compute(solve func([][]int) ([]int, int), distances [][]int, name string) t
 	elapsed := time.Since(start)
 	bestResult := max(results)
 	meanResult := mean(results)
+
 	stdResult := std(results)
 	meanSteps := mean(stepCounts)
 
-	fmt.Println(name, elapsed, bestResult, meanResult, meanSteps, stdResult)
+	fmt.Println(name, "elapsed: ", elapsed, "best: ", bestResult, "mean: ", meanResult, "meas steps: ", meanSteps, "std result: ", stdResult)
 	return elapsed
 }
 
@@ -57,6 +94,6 @@ func computeRandom(distances [][]int, availableTime time.Duration) time.Duration
 	result := getDistance(permutation, distances)
 	elapsed := time.Since(start)
 
-	fmt.Println("Random", elapsed, result)
+	fmt.Println("Random, elapsed: ", elapsed, "result: ", result)
 	return elapsed
 }
